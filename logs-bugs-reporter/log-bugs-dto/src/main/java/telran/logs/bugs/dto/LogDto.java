8797d0689d68c57c6a63528c4bae2fd510610a28
Yuragranovsky <yuragranovsky@gmail.com>
2021-01-17 20:53:49 +0200

    Added LogDto module with Unit Test

---
+++ logs-bugs-reporter/log-bugs-dto/src/main/java/telran/logs/bugs/dto/LogDto.java
@@ -0,0 +1,69 @@
+package telran.logs.bugs.dto;
+
+import java.util.Date;
+
+import javax.validation.constraints.*;
+
+public class LogDto {
+	public LogDto(@NotNull Date dateTime, @NotNull LogType logType, @NotEmpty String artifact, int responseTime,
+			String result) {
+		super();
+		this.dateTime = dateTime;
+		this.logType = logType;
+		this.artifact = artifact;
+		this.responseTime = responseTime;
+		this.result = result;
+	}
+	@NotNull
+	public Date dateTime;
+	@NotNull
+	public LogType logType;
+	@NotEmpty
+	public String artifact;
+	public int responseTime;
+	public String result;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((artifact == null) ? 0 : artifact.hashCode());
+		result = prime * result + ((dateTime == null) ? 0 : dateTime.hashCode());
+		result = prime * result + ((logType == null) ? 0 : logType.hashCode());
+		result = prime * result + responseTime;
+		result = prime * result + ((this.result == null) ? 0 : this.result.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LogDto other = (LogDto) obj;
+		if (artifact == null) {
+			if (other.artifact != null)
+				return false;
+		} else if (!artifact.equals(other.artifact))
+			return false;
+		if (dateTime == null) {
+			if (other.dateTime != null)
+				return false;
+		} else if (!dateTime.equals(other.dateTime))
+			return false;
+		if (logType != other.logType)
+			return false;
+		if (responseTime != other.responseTime)
+			return false;
+		if (result == null) {
+			if (other.result != null)
+				return false;
+		} else if (!result.equals(other.result))
+			return false;
+		return true;
+	}
+	
+	
+
+}
---
+++ logs-bugs-reporter/log-bugs-dto/src/test/java/telran/logs/bugs/dto/LogDtoTest.java
@@ -0,0 +1,51 @@
+package telran.logs.bugs.dto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.util.Date;
+
+import javax.validation.Valid;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+@WebMvcTest(LogDtoTest.TestController.class)
+@ContextConfiguration(classes=LogDtoTest.TestController.class)
+public class LogDtoTest {
+public static @RestController class TestController {
+	static LogDto logDtoExp = new LogDto(new Date(), LogType.NO_EXCEPTION,
+			"artifact", 0, "");
+	@PostMapping("/")
+	void testPost(@RequestBody @Valid LogDto logDto) {
+		assertEquals(logDtoExp, logDto);
+	}
+}
+ObjectMapper mapper = new ObjectMapper();
+@Autowired
+MockMvc mock;
+@Test
+void testPostRun() throws JsonProcessingException, Exception {
+	assertEquals(200, mock.perform(post("/")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(mapper.writeValueAsString(TestController.logDtoExp)))
+			.andReturn()
+			.getResponse().getStatus());
+}
+}
