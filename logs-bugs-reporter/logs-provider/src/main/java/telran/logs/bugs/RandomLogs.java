d9070a1c8e89a852860a0e2cf9c99dc7a3ed26c9
Yuragranovsky <yuragranovsky@gmail.com>
2021-01-24 16:42:51 +0200

    HW-57 RandomLogs with tests per https://www.dropbox.com/scl/fi/gbgnmrngxrcsh8ol7l02x/57-RandomLogsCreation.docx?dl=0&rlkey=jozt8p3sntczqyx96y4mc3np3

---
+++ logs-bugs-reporter/logs-provider/src/main/java/telran/logs/bugs/RandomLogs.java
@@ -0,0 +1,99 @@
+package telran.logs.bugs;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+import org.springframework.stereotype.Component;
+
+import telran.logs.bugs.dto.LogDto;
+import telran.logs.bugs.dto.LogType;
+
+@Component
+public class RandomLogs {
+	int secExceptionProb = 30;
+	int exceptionProb = 10;
+	int authenticationProb = 70;
+public LogDto createRandomLog() {
+	LogType logType = getLogType();
+	return new LogDto(new Date(), logType, getArtifact(logType), getResponseTime(logType), "");
+}
+
+private int getResponseTime(LogType logType) {
+	
+	return logType == LogType.NO_EXCEPTION ?
+			ThreadLocalRandom.current().nextInt(20, 200):0;
+}
+
+private  String getArtifact(LogType logType) {
+	EnumMap<LogType, String> logArtifact = getLogArtifactMap();
+	return logArtifact.get(logType);
+}
+
+private EnumMap<LogType, String> getLogArtifactMap() {
+	EnumMap<LogType, String> res = new EnumMap<>(LogType.class);
+	Arrays.asList(LogType.values()).forEach(lt -> {
+		fillLogTypeArtifactMap(res, lt);
+	});
+	return res;
+}
+
+private void fillLogTypeArtifactMap(EnumMap<LogType, String> res, LogType lt) {
+	switch(lt) {
+	case AUTHENTICATION_EXCEPTION: 
+		res.put(LogType.AUTHENTICATION_EXCEPTION, "authentication");
+		break;
+	case AUTHORIZATION_EXCEPTION:
+		res.put(LogType.AUTHORIZATION_EXCEPTION, "authorization");
+		break;
+	case BAD_REQUEST_EXCEPTION:
+		res.put(LogType.BAD_REQUEST_EXCEPTION, "class");
+		break;
+	case DUPLICATED_KEY_EXCEPTION:
+		res.put(LogType.DUPLICATED_KEY_EXCEPTION, "class");
+		break;
+	case  NOT_FOUND_EXCEPTION:
+		res.put(LogType. NOT_FOUND_EXCEPTION, "class");
+		break;
+	case NO_EXCEPTION:
+		res.put(LogType. NO_EXCEPTION, "class");
+		break;
+	case SERVER_EXCEPTION:
+		res.put(LogType. SERVER_EXCEPTION, "class");
+		break;
+	
+	
+	}
+}
+
+
+private LogType getLogType() {
+	int chance = getChance();
+	
+	return chance <= exceptionProb ? getExceptionLog() : LogType.NO_EXCEPTION;
+}
+
+private LogType getExceptionLog() {
+	
+	return getChance() <= secExceptionProb ? getSecurityExceptionLog() : getNonSecurityExceptionLog();
+}
+
+private LogType getNonSecurityExceptionLog() {
+	LogType nonSecExceptions[] = {
+			LogType.BAD_REQUEST_EXCEPTION, LogType.DUPLICATED_KEY_EXCEPTION,
+			LogType.NOT_FOUND_EXCEPTION, LogType.SERVER_EXCEPTION
+	};
+	int ind = ThreadLocalRandom.current().nextInt(0, nonSecExceptions.length);
+	return nonSecExceptions[ind];
+}
+
+private LogType getSecurityExceptionLog() {
+	
+	return getChance() <= authenticationProb ? LogType.AUTHENTICATION_EXCEPTION : LogType.AUTHENTICATION_EXCEPTION ;
+}
+
+private int getChance() {
+	
+	return ThreadLocalRandom.current().nextInt(1, 101);
+}
+}
---
+++ logs-bugs-reporter/logs-provider/src/test/java/teltan/logs/bugs/RandomLogsTest.java
@@ -0,0 +1,107 @@
+package teltan.logs.bugs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import telran.logs.bugs.RandomLogs;
+import telran.logs.bugs.dto.*;
+
+@ExtendWith(SpringExtension.class)
+@EnableAutoConfiguration
+@ContextConfiguration(classes = RandomLogs.class)
+public class RandomLogsTest {
+	private static final String AUTHENTICATION_ARTIFACT = "authentication";
+	private static final String AUTHORIZATION_ARTIFACT = "authorization";
+	private static final String CLASS_ARTIFACT = "class";
+	private static final long N_LOGS = 100000;
+	@Autowired
+	RandomLogs randomLogs;
+	 
+	@Test
+	void logTypeArtifactTest() throws Exception {
+		
+		EnumMap<LogType, String> logTypeArtifactsMap = getMapForTest();
+		logTypeArtifactsMap.forEach((k, v) -> {
+			switch (k) {
+			case AUTHENTICATION_EXCEPTION:
+				assertEquals(AUTHENTICATION_ARTIFACT, v);
+				break;
+			case AUTHORIZATION_EXCEPTION:
+				assertEquals(AUTHORIZATION_ARTIFACT, v);
+				break;
+			default:
+				assertEquals(CLASS_ARTIFACT, v);
+			
+			}
+		});
+	}
+
+	private EnumMap<LogType, String> getMapForTest()
+			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		Method getMapMethod = randomLogs.getClass()
+				.getDeclaredMethod("getLogArtifactMap");
+		getMapMethod.setAccessible(true);
+		@SuppressWarnings("unchecked")
+		EnumMap<LogType, String> logTypeArtifactsMap =
+				(EnumMap<LogType, String>) getMapMethod.invoke(randomLogs);
+		return logTypeArtifactsMap;
+	}
+	@Test
+	void generation() throws Exception{
+		
+		List<LogDto> logs =Stream
+				.generate(() -> randomLogs.createRandomLog()).limit(N_LOGS)
+				.collect(Collectors.toList());
+		testLogContent(logs);
+		Map<LogType, Long> logTypeOccurrences = 
+				logs.stream().collect(Collectors.groupingBy(l -> l.logType, Collectors.counting()));
+		logTypeOccurrences.forEach((k, v) -> {
+			System.out.println();
+		});
+		
+	}
+
+	private void testLogContent(List<LogDto> logs) {
+		logs.forEach(log -> {
+			switch (log.logType) {
+			case AUTHENTICATION_EXCEPTION:
+				assertEquals(AUTHENTICATION_ARTIFACT, log.artifact);
+				assertEquals(0, log.responseTime);
+				assertTrue(log.result.isEmpty());
+				break;
+			case AUTHORIZATION_EXCEPTION:
+				assertEquals(AUTHORIZATION_ARTIFACT, log.artifact);
+				assertEquals(0, log.responseTime);
+				assertTrue(log.result.isEmpty());
+				break;
+			
+			case NO_EXCEPTION:
+				assertEquals(CLASS_ARTIFACT, log.artifact);
+				assertTrue(log.responseTime > 0);
+				assertTrue(log.result.isEmpty());
+				break;
+			
+			default:
+				assertEquals(CLASS_ARTIFACT, log.artifact);
+				assertEquals(0, log.responseTime);
+				assertTrue(log.result.isEmpty());
+				break;
+			
+			}
+		});
+	}
+
+}
