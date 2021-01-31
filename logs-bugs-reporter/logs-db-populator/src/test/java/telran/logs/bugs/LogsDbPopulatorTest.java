f89d5608cae526d854e2adbcdc90462e58b8e900
Yuragranovsky <yuragranovsky@gmail.com>
2021-01-31 19:04:00 +0200

    HW-59 Fix bug with embedded MongoDB

--- logs-bugs-reporter/logs-db-populator/src/test/java/telran/logs/bugs/LogsDbPopulatorTest.java
+++ logs-bugs-reporter/logs-db-populator/src/test/java/telran/logs/bugs/LogsDbPopulatorTest.java
@@ -3,8 +3,6 @@ package telran.logs.bugs;
 import static org.assertj.core.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Date;
 import java.util.List;
@@ -17,7 +15,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.*;
 import org.springframework.context.annotation.Import;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 
 import telran.logs.bugs.dto.LogDto;
@@ -64,7 +61,7 @@ public class LogsDbPopulatorTest {
 		assertEquals(1, logDocs.size());
 		LogDto logDto = logDocs.get(0).getLogDto();
 		assertEquals(LogType.BAD_REQUEST_EXCEPTION, logDto.logType);
-		assertEquals(LogsDbPopulatorAppl.class.getCanonicalName(), logDto.artifact);
+		assertEquals(LogsDbPopulatorAppl.class.getName(), logDto.artifact);
 		assertEquals(0, logDto.responseTime);
 		assertFalse(logDto.result.isEmpty());
 		try {
