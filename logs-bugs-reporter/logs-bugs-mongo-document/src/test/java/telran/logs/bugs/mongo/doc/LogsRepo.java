e5edf8f0bf099e3879ae9c41c34d98bc1b0f8c83
Yuragranovsky <yuragranovsky@gmail.com>
2021-02-10 19:30:23 +0200

    CW-63 introducing reactive spring data mongodb

--- logs-bugs-reporter/logs-bugs-mongo-document/src/test/java/telran/logs/bugs/mongo/doc/LogsRepo.java
+++ logs-bugs-reporter/logs-bugs-mongo-document/src/test/java/telran/logs/bugs/mongo/doc/LogsRepo.java
@@ -2,8 +2,9 @@ package telran.logs.bugs.mongo.doc;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface LogsRepo extends MongoRepository<LogDoc, ObjectId> {
+public interface LogsRepo extends ReactiveMongoRepository<LogDoc, ObjectId> {
 
 }
 
